69bf82f53959aa7e508cad269af4ca472f0bec4c
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2023-05-15 18:02:11 -0500

    Update Python index add python.js

---
+++ src/python.js
@@ -0,0 +1,22 @@
+let VLO = ''
+
+function python(vlo){
+    VLO = vlo
+    Konekti.uses('hypermedia')
+}
+
+function KonektiMain(){
+    var text = {'plugin':'latex', 'setup':["info", VLO.info, {'style':'width:100%;height:fit;'}]}
+    var audio = VLO.audio
+    if(audio!==undefined) audio = {'plugin':'media', 'setup':["mediacontrol", "audio", "mp3", VLO.audio]}
+    var left = (audio!=undefined)?[audio,text]:[text]
+    left = {'plugin':'raw', 'setup':['explain', left, {'style':'width:100%;height:100%;'}]}
+    var viewer = {'plugin':'python', 'setup':["viewer", VLO.ide.server, 
+        VLO.modules, {'header':{'title':VLO.ide.title}, 'layout':'row', 'terminal':{'class':' w3-black '}, 'main':{'class':'w3-teal', 'style':'width:100%;height:100%;'}}]}
+
+    var control = {'plugin':'split', 'setup':['coderviewer', 'col', 50, left, viewer, {'style':'width:100%;height:100%;'}]}
+
+    var scripts = VLO.scripts
+
+    Konekti.hypermedia('book', control, 'mediacontrol', scripts, {'style':'width:100%;height:100%;'})
+}
